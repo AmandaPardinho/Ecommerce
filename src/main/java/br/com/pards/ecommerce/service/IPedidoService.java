52d1e2c07d682031cb02c63d75393433bbf43434
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-30 20:09:59 -0300

    IPedidoService interface and its code were created

--- src/main/java/br/com/pards/ecommerce/service/IPedidoService.java
+++ src/main/java/br/com/pards/ecommerce/service/IPedidoService.java
@@ -1,2 +1,9 @@
-package br.com.pards.ecommerce.service;public interface IPedidoService {
+package br.com.pards.ecommerce.service;
+
+import br.com.pards.ecommerce.model.Pedido;
+
+public interface IPedidoService {
+
+    public Pedido inserirNovoPedido(Pedido novo);
+    public Pedido recuperarPeloNumero(Integer numero);
 }
