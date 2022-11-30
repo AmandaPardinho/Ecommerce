8502442118b4d45021603c512761d2ec0ec57081
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-30 20:09:08 -0300

    IProduto Service interface and its code were created

--- src/main/java/br/com/pards/ecommerce/service/IProdutoService.java
+++ src/main/java/br/com/pards/ecommerce/service/IProdutoService.java
@@ -1,4 +1,11 @@
 package br.com.pards.ecommerce.service;
 
+import br.com.pards.ecommerce.model.Produto;
+
+import java.util.ArrayList;
+
 public interface IProdutoService {
+    public ArrayList<Produto> recuperarTodos();
+    public Produto recuperarPeloCodigo(Integer codigo);
+    public Produto cadastrarNovo(Produto novo);
 }
