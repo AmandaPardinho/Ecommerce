6ba4ebb0d276043c6be2967145c2eb88f69f4658
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-30 20:08:09 -0300

    PedidoServiceImpl class and its code were created

--- src/main/java/br/com/pards/ecommerce/service/PedidoServiceImpl.java
+++ src/main/java/br/com/pards/ecommerce/service/PedidoServiceImpl.java
@@ -1,2 +1,28 @@
-package br.com.pards.ecommerce.service;public class PedidoServiceImpl {
+package br.com.pards.ecommerce.service;
+
+import br.com.pards.ecommerce.dao.PedidoDAO;
+import br.com.pards.ecommerce.model.ItemPedido;
+import br.com.pards.ecommerce.model.Pedido;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoServiceImpl implements IPedidoService{
+
+    @Autowired
+    private PedidoDAO dao;
+
+    @Override
+    public Pedido inserirNovoPedido(Pedido novo) {
+        //antes de fazer um SAVE. é preciso indicar aos "filhos" (itens) a qual pedido eles pertencem
+        for(ItemPedido item: novo.getItens()){
+            item.setPedido(novo);
+        }
+        return dao.save(novo);
+    }
+
+    @Override
+    public Pedido recuperarPeloNumero(Integer numero) {
+        return dao.findById(numero).orElse(null);
+    }
 }
