afb6ed5bc348a5507a8e5d5cd32f2d3e89559ee4
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-30 20:17:12 -0300

    ProductController and its code were created

--- src/main/java/br/com/pards/ecommerce/controller/ProdutoController.java
+++ src/main/java/br/com/pards/ecommerce/controller/ProdutoController.java
@@ -1,2 +1,43 @@
-package br.com.pards.ecommerce.controller;public class ProdutoController {
+package br.com.pards.ecommerce.controller;
+
+import br.com.pards.ecommerce.model.Produto;
+import br.com.pards.ecommerce.service.IProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+public class ProdutoController {
+
+    @Autowired
+    private IProdutoService service;
+
+    //recuperar tudo
+    @GetMapping
+    public ArrayList<Produto> recuperarTodoMundo(){
+        return service.recuperarTodos();
+    }
+
+    //recuperar pelo id
+    @GetMapping("/produtos/{codigo}")
+    public ResponseEntity<Produto> recuperarPeloCodigo(@PathVariable Integer codigo){
+        Produto res = service.recuperarPeloCodigo(codigo);
+        if(res != null){
+            return ResponseEntity.ok(res);
+        }
+        return ResponseEntity.status(404).build();
+    }
+
+    //cadastrar
+    @PostMapping("/produtos")
+    public ResponseEntity<Produto> inserirNovoProduto(@RequestBody Produto novo){
+        Produto res = service.cadastrarNovo(novo);
+        if(res != null){
+            return ResponseEntity.ok(res);
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
 }
