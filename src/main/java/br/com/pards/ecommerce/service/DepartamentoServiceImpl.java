1fe585a5968e9f0530b5a0477c958d41e4583eb0
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-30 15:02:58 -0300

    Creation of the DepartamentoServiceImpl and its code

--- src/main/java/br/com/pards/ecommerce/service/DepartamentoServiceImpl.java
+++ src/main/java/br/com/pards/ecommerce/service/DepartamentoServiceImpl.java
@@ -1,2 +1,47 @@
-package br.com.pards.ecommerce.service;public class DepartamentoServiceImpl {
+package br.com.pards.ecommerce.service;
+
+import br.com.pards.ecommerce.dao.DepartamentoDAO;
+import br.com.pards.ecommerce.model.Departamento;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@Service
+public class DepartamentoServiceImpl implements IDepartamentoService{
+    @Autowired
+    private DepartamentoDAO dao;
+
+    @Override
+    public Departamento criarNovo(Departamento novo) {
+        if(novo.getNome() != null){
+            return dao.save(novo);
+        }
+        return null;
+    }
+
+    @Override
+    public Departamento atualizarDados(Departamento dados) {
+        if(dados.getNumero() != null && dados.getNome() != null){
+            return dao.save(dados);
+        }
+        return null;
+    }
+
+    @Override
+    public ArrayList<Departamento> buscarTodos() {
+        return (ArrayList<Departamento>) dao.findAll();
+    }
+
+    @Override
+    public Departamento buscarPeloId(Integer id) {
+        return dao.findById(id).orElse(null);
+    }
+
+    @Override
+    public void excluirDepartamento(Integer id) {
+        dao.deleteById(id);
+    }
 }
