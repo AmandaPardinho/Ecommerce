a8e322885a371b5f05817c31dd1bb6de30a148e6
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-30 20:16:04 -0300

    PedidoDAO interface and its code were created

--- src/main/java/br/com/pards/ecommerce/dao/PedidoDAO.java
+++ src/main/java/br/com/pards/ecommerce/dao/PedidoDAO.java
@@ -1,2 +1,8 @@
-package br.com.pards.ecommerce.dao;public interface PedidoDAO {
+package br.com.pards.ecommerce.dao;
+
+import br.com.pards.ecommerce.model.Pedido;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PedidoDAO extends CrudRepository<Pedido, Integer> {
+
 }
