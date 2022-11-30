579f39cb311a5d232b0adf025bb48ab7eab54ad5
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-30 20:11:44 -0300

    ItemPedido class and its code were created

--- src/main/java/br/com/pards/ecommerce/model/ItemPedido.java
+++ src/main/java/br/com/pards/ecommerce/model/ItemPedido.java
@@ -1,2 +1,81 @@
-package br.com.pards.ecommerce.model;public class ItemPedido {
+package br.com.pards.ecommerce.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item_pedido")
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sequencial", nullable = false)
+    private Integer sequencial;
+
+    @Column(name = "quantidade")
+    private Integer quantidade;
+
+    @Column(name = "preco_unitario")
+    private Double precoUnitario;
+
+    @Column(name = "preco_final")
+    private Double precoFinal;
+
+    @ManyToOne
+    @JoinColumn(name = "codigo_produto")
+    private Produto produto;
+
+    @ManyToOne
+    @JoinColumn(name = "numero_pedido")
+    @JsonIgnoreProperties("itens")
+    private Pedido pedido;
+
+    public Integer getSequencial() {
+        return sequencial;
+    }
+
+    public void setSequencial(Integer sequencial) {
+        this.sequencial = sequencial;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(Double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public Double getPrecoFinal() {
+        return precoFinal;
+    }
+
+    public void setPrecoFinal(Double precoFinal) {
+        this.precoFinal = precoFinal;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
 }
