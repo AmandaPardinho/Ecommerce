b4510b3817b7df8afb3fbe9e26687e132a61ea34
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-30 20:14:16 -0300

    Cliente class and its code were created

--- src/main/java/br/com/pards/ecommerce/model/Cliente.java
+++ src/main/java/br/com/pards/ecommerce/model/Cliente.java
@@ -1,2 +1,65 @@
-package br.com.pards.ecommerce.model;public class Cliente {
+package br.com.pards.ecommerce.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "email", length = 70, nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "senha", length = 20, nullable = false)
+    private String senha;
+
+    @Column(name = "cpf", length = 15, nullable = false, unique = true)
+    private String cpf;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
