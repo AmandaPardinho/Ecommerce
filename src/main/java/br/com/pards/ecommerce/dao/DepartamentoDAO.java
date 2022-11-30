d727850ca9d39ff94b165897ad6cacbe40b1b4a1
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-30 15:06:56 -0300

    Creating the DepartamentoDAO interface and its code

--- src/main/java/br/com/pards/ecommerce/dao/DepartamentoDAO.java
+++ src/main/java/br/com/pards/ecommerce/dao/DepartamentoDAO.java
@@ -1,2 +1,10 @@
-package br.com.pards.ecommerce.dao;public interface DepartamentoDAO {
+package br.com.pards.ecommerce.dao;
+
+import br.com.pards.ecommerce.model.Departamento;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EntityScan(basePackages = {"br.com.pards.ecommerce.dao.DepartamentoDAO"})
+public interface DepartamentoDAO extends CrudRepository<Departamento, Integer> {
+
 }
