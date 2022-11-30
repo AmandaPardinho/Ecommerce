20ee45da79072afcabe0e915d6c2493209a3dc72
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-30 15:01:38 -0300

    Creation of the IDepartamentoService interface and its code

--- src/main/java/br/com/pards/ecommerce/service/IDepartamentoService.java
+++ src/main/java/br/com/pards/ecommerce/service/IDepartamentoService.java
@@ -1,2 +1,13 @@
-package br.com.pards.ecommerce.service;public interface IDepartamentoService {
+package br.com.pards.ecommerce.service;
+
+import br.com.pards.ecommerce.model.Departamento;
+
+import java.util.ArrayList;
+
+public interface IDepartamentoService {
+    public Departamento criarNovo(Departamento novo);
+    public Departamento atualizarDados(Departamento dados);
+    public ArrayList<Departamento> buscarTodos();
+    public Departamento buscarPeloId(Integer id);
+    public void excluirDepartamento(Integer id);
 }
