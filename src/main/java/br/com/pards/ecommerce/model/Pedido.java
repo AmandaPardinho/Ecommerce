62e4b44a3db314e54aee395872b4ed6c289ff9e7
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-30 20:11:12 -0300

    Pedido class and its code were created

--- src/main/java/br/com/pards/ecommerce/model/Pedido.java
+++ src/main/java/br/com/pards/ecommerce/model/Pedido.java
@@ -1,2 +1,107 @@
-package br.com.pards.ecommerce.model;public class Pedido {
+package br.com.pards.ecommerce.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "numero", nullable = false)
+    private Integer numero;
+
+    @Column(name = "status", length = 1, nullable = false)
+    private String status;
+
+    @Column(name = "data_pedido")
+    private LocalDate data;
+
+    @Column(name = "valor_bruto")
+    private Double valorBruto;
+
+    @Column(name = "desconto")
+    private Double desconto;
+
+    @Column(name = "valor_liquido")
+    private Double valorFinal;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente")
+    private Cliente cliente;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("pedido")
+    private List<ItemPedido> itens;
+
+    public Integer getNumero() {
+
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+
+        this.numero = numero;
+    }
+
+    public String getStatus() {
+
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Double getValorBruto() {
+        return valorBruto;
+    }
+
+    public void setValorBruto(Double valorBruto) {
+        this.valorBruto = valorBruto;
+    }
+
+    public Double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(Double desconto) {
+        this.desconto = desconto;
+    }
+
+    public Double getValorFinal() {
+        return valorFinal;
+    }
+
+    public void setValorFinal(Double valorFinal) {
+        this.valorFinal = valorFinal;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemPedido> itens) {
+        this.itens = itens;
+    }
 }
