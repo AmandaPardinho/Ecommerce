a39c164f7ae07eac64c836a63bdf590a71ce7885
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-30 20:17:45 -0300

    PedidoController and its code were created

--- src/main/java/br/com/pards/ecommerce/controller/PedidoController.java
+++ src/main/java/br/com/pards/ecommerce/controller/PedidoController.java
@@ -1,2 +1,29 @@
-package br.com.pards.ecommerce.controller;public class PedidoController {
+package br.com.pards.ecommerce.controller;
+
+import br.com.pards.ecommerce.model.Pedido;
+import br.com.pards.ecommerce.service.IPedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class PedidoController {
+
+    @Autowired
+    private IPedidoService service;
+
+    @GetMapping("/pedidos/{numero}")
+    public ResponseEntity<Pedido> recuperarPeloNumero(@PathVariable Integer numero){
+        Pedido res = service.recuperarPeloNumero(numero);
+        if(res != null){
+            return ResponseEntity.ok(res);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/pedidos")
+    public ResponseEntity<Pedido> inserirNovoPedido(@RequestBody Pedido novo){
+        return ResponseEntity.ok(service.inserirNovoPedido(novo));
+    }
+
 }
