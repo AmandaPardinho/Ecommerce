a7a9c360f628eb5dbd037a9e15ac495514fbb532
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-30 15:08:03 -0300

    Creating the DepartamentoController class and its code

--- src/main/java/br/com/pards/ecommerce/controller/DepartamentoController.java
+++ src/main/java/br/com/pards/ecommerce/controller/DepartamentoController.java
@@ -1,2 +1,55 @@
-package br.com.pards.ecommerce.controller;public class DepartamentoController {
+package br.com.pards.ecommerce.controller;
+
+import br.com.pards.ecommerce.dao.DepartamentoDAO;
+import br.com.pards.ecommerce.model.Departamento;
+import br.com.pards.ecommerce.service.IDepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+public class DepartamentoController {
+
+    @Autowired  //faz a injecao de dependencias
+    private IDepartamentoService service;
+
+    @GetMapping("/departamentos")
+    public ArrayList<Departamento> recuperarTodos(){
+        return service.buscarTodos();
+    }
+
+    @PostMapping("/departamentos")
+    public ResponseEntity<Departamento> incluirNovo(@RequestBody Departamento novo){
+        Departamento res = service.criarNovo(novo);
+        if(res != null){
+            return ResponseEntity.ok(res);
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PutMapping("/departamentos")
+    public ResponseEntity<Departamento> alterar(@RequestBody Departamento dados){
+        Departamento res = service.atualizarDados(dados);
+        if(res != null){
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @DeleteMapping("/departamentos/{id}")
+    public ResponseEntity<Departamento> excluirDepartamento(@PathVariable Integer id){
+        service.excluirDepartamento(id);
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/departamentos/{numero}")
+    public ResponseEntity<Departamento> buscarPeloId(@PathVariable Integer numero){
+        Departamento res = service.buscarPeloId(numero);
+        if(res != null){
+            return ResponseEntity.ok(res);
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
