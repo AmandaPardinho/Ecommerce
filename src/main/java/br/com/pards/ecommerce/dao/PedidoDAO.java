c76beb4a85f88e73c814a8804fdddf60cd13bc9e
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-30 22:34:23 -0300

    PedidoDAO class has been updated

--- src/main/java/br/com/pards/ecommerce/dao/PedidoDAO.java
+++ src/main/java/br/com/pards/ecommerce/dao/PedidoDAO.java
@@ -1,8 +1,21 @@
 package br.com.pards.ecommerce.dao;
 
+import br.com.pards.ecommerce.dto.FaturamentoMensal;
 import br.com.pards.ecommerce.model.Pedido;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.ArrayList;
 
 public interface PedidoDAO extends CrudRepository<Pedido, Integer> {
+
+    @Query("SELECT new "
+            + "br.com.pards.ecommerce.dto.FaturamentoMensal(munth(p.data), sum(p.valorFinal) "
+            + "FROM Pedido p "
+            + "WHERE year(p.data) = :ano "
+            + "GROUP BY month(p.data) "
+            + "ORDER BY month(p.data) ")
+    public ArrayList<FaturamentoMensal> recuperarFaturamentoPorAno(@Param("ano") Integer ano);
 
 }
