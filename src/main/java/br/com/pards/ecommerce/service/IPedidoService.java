6757548cd6d2b529c4faa447d010c1a0243e5b42
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-30 22:30:28 -0300

    IPedidosService class has been updated

--- src/main/java/br/com/pards/ecommerce/service/IPedidoService.java
+++ src/main/java/br/com/pards/ecommerce/service/IPedidoService.java
@@ -1,9 +1,13 @@
 package br.com.pards.ecommerce.service;
 
+import br.com.pards.ecommerce.dto.FaturamentoMensal;
 import br.com.pards.ecommerce.model.Pedido;
+
+import java.util.ArrayList;
 
 public interface IPedidoService {
 
     public Pedido inserirNovoPedido(Pedido novo);
     public Pedido recuperarPeloNumero(Integer numero);
+    public ArrayList<FaturamentoMensal> recuperarFaturamento(Integer ano);
 }
