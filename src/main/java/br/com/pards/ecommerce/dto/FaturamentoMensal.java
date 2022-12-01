310f78b427ef3acac70dce10f78be23db2ee2ed7
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-30 22:32:29 -0300

    FaturamentoMensal and its code were created

--- src/main/java/br/com/pards/ecommerce/dto/FaturamentoMensal.java
+++ src/main/java/br/com/pards/ecommerce/dto/FaturamentoMensal.java
@@ -1,2 +1,28 @@
-package br.com.pards.ecommerce.dto;public class FaturamentoMensal {
+package br.com.pards.ecommerce.dto;
+
+public class FaturamentoMensal {
+    private Integer mes;
+    private Double valorTotal;
+
+    public FaturamentoMensal(Integer mes, Double valorTotal) {
+        super();
+        this.mes = mes;
+        this.valorTotal = valorTotal;
+    }
+
+    public Integer getMes() {
+        return mes;
+    }
+
+    public void setMes(Integer mes) {
+        this.mes = mes;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
 }
