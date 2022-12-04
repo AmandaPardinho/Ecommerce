06335a28818d76d7036a5c7266c961c345a07c77
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-04 18:24:47 -0300

    Made the imports again and commented the query

--- src/main/java/br/com/pards/ecommerce/dao/PedidoDAO.java
+++ src/main/java/br/com/pards/ecommerce/dao/PedidoDAO.java
@@ -1,5 +1,6 @@
 package br.com.pards.ecommerce.dao;
 
+
 import br.com.pards.ecommerce.dto.FaturamentoMensal;
 import br.com.pards.ecommerce.model.Pedido;
 import org.springframework.data.jpa.repository.Query;
@@ -10,12 +11,12 @@ import java.util.ArrayList;
 
 public interface PedidoDAO extends CrudRepository<Pedido, Integer> {
 
-    @Query("SELECT new "
+    /*@Query("SELECT new "
             + "br.com.pards.ecommerce.dto.FaturamentoMensal(munth(p.data), sum(p.valorFinal) "
             + "FROM Pedido p "
             + "WHERE year(p.data) = :ano "
             + "GROUP BY month(p.data) "
             + "ORDER BY month(p.data) ")
-    public ArrayList<FaturamentoMensal> recuperarFaturamentoPorAno(@Param("ano") Integer ano);
+    public ArrayList<FaturamentoMensal> recuperarFaturamentoPorAno(@Param("ano") Integer ano);*/
 
 }
