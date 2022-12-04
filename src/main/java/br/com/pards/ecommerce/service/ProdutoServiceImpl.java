5ccdbec222231912e438d11aab4b77095a6cdb62
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-04 16:59:25 -0300

    Made the imports again and changed some methods

--- src/main/java/br/com/pards/ecommerce/service/ProdutoServiceImpl.java
+++ src/main/java/br/com/pards/ecommerce/service/ProdutoServiceImpl.java
@@ -1,5 +1,6 @@
 package br.com.pards.ecommerce.service;
 
+
 import br.com.pards.ecommerce.dao.ProdutoDAO;
 import br.com.pards.ecommerce.model.Produto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,13 @@ public class ProdutoServiceImpl implements IProdutoService{
     @Autowired
     private ProdutoDAO dao;
 
-    @Override
-    public ArrayList<Produto> recuperarTodos() {
-//        return (ArrayList<Produto>) dao.findAll();
-//        return dao.findByOrderByPreco();
-//        return dao.findByOrderByPrecoDesc();
-        return dao.findByNameContaining("Console");
-    }
+     @Override
+     public ArrayList<Produto> recuperarTodos(){
+        /* return (ArrayList<Produto>) dao.findAll();
+        return dao.findByOrderByPreco();
+        return dao.findByOrderByPrecoDesc();*/
+        return dao.findByNomeContaining("Console");
+     }
     @Override
     public Produto recuperarPeloCodigo(Integer codigo) {
 
@@ -34,6 +35,6 @@ public class ProdutoServiceImpl implements IProdutoService{
 
     @Override
     public ArrayList<Produto> buscarPorPalavraChave(String palavra) {
-        return dao.findByNameContaining(palavra);
-    }
+        return dao.findByNomeContaining(palavra);
+   }
 }
