71bb0373771b4d129a425f254d54fc8e35490e81
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-04 18:20:47 -0300

    Made the imports again and changed the name of the method to findByNomeContaining

--- src/main/java/br/com/pards/ecommerce/dao/ProdutoDAO.java
+++ src/main/java/br/com/pards/ecommerce/dao/ProdutoDAO.java
@@ -1,15 +1,16 @@
 package br.com.pards.ecommerce.dao;
 
+
 import br.com.pards.ecommerce.model.Produto;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.ArrayList;
 
-public interface ProdutoDAO extends CrudRepository<Produto, Integer>{
+public interface ProdutoDAO extends CrudRepository<Produto, Integer> {
 
     public ArrayList<Produto> findByOrderByPreco();
     public ArrayList<Produto> findByOrderByPrecoDesc();
-    public ArrayList<Produto> findByNameContaining(String palavraChave);
+    public ArrayList<Produto> findByNomeContaining(String palavraChave);
 
 
 }
