a6664df50195fa565da729e07d7ebd2a53247675
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-04 18:23:19 -0300

    Made the imports again and deleted EntityScan

--- src/main/java/br/com/pards/ecommerce/dao/DepartamentoDAO.java
+++ src/main/java/br/com/pards/ecommerce/dao/DepartamentoDAO.java
@@ -1,10 +1,9 @@
 package br.com.pards.ecommerce.dao;
 
+
 import br.com.pards.ecommerce.model.Departamento;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.repository.CrudRepository;
 
-@EntityScan(basePackages = {"br.com.pards.ecommerce.dao.DepartamentoDAO"})
 public interface DepartamentoDAO extends CrudRepository<Departamento, Integer> {
 
 }
